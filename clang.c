c366985de5f8c119d0b5f10ad7062f96132c3a3e
eileenqueen <62704369+eileenqueen@users.noreply.github.com>
2020-04-15 22:43:50 -0400

    Create clang.c

---
+++ clang.c
@@ -0,0 +1,15 @@
+//Insert comments into C with double backslashes!
+
+//If this were a real C program, we might want to include data from other files. We can link them with:
+#include <filename.h>
+
+//Then, we need to declare the main function
+int main(void)
+//everything that the computer needs to execute beyond this needs to be enclosed in curly braces.
+{
+  //now that the runction is rolling, we can start doing things!
+  //the function "printf" will print some text to the user's screen using the following syntax:
+  printf("This is my cheat sheet for C, which I learned a bit about during week 1 of CS50.\n")
+  printf("I decided to make a cheat sheet because C was much less intuitive for me than other languages have been.\n")
+  //the "\n" at the end means that the prompt will go to the next line, like hitting the 'enter' key.
+}
